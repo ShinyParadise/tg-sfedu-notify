b439b0ba6bcf0e6a301e13ee85bf5a358960ce4c
ShinyParadise <shinodise@gmail.com>
2023-01-14 21:07:07 +0300

    New method

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,9 +2,14 @@ import dev.inmo.tgbotapi.extensions.api.bot.getMe
 import dev.inmo.tgbotapi.extensions.api.send.reply
 import dev.inmo.tgbotapi.extensions.api.telegramBot
 import dev.inmo.tgbotapi.extensions.behaviour_builder.buildBehaviourWithLongPolling
+import dev.inmo.tgbotapi.extensions.behaviour_builder.expectations.waitText
 import dev.inmo.tgbotapi.extensions.behaviour_builder.triggers_handling.onCommand
+import dev.inmo.tgbotapi.requests.send.SendTextMessage
+import dev.inmo.tgbotapi.utils.PreviewFeature
+import kotlinx.coroutines.flow.first
 import java.io.File
 
+@OptIn(PreviewFeature::class)
 suspend fun main() {
     val token = File("src/main/resources/token.txt").readText(Charsets.UTF_8)
 
@@ -13,8 +18,21 @@ suspend fun main() {
     bot.buildBehaviourWithLongPolling {
         println(getMe())
 
-        onCommand("start") {
-            reply(it, "Hi :)")
+        onCommand("start", requireOnlyCommandInMessage = true) {
+            reply(it, "Привет :)")
         }
+
+        onCommand("setgroup", requireOnlyCommandInMessage = true) {
+            val group = waitText(
+                SendTextMessage(
+                    it.chat.id,
+                    "Введи свою группу как в зачетке."
+                )
+            ).first().text
+
+            println(group)
+
+        }
+
     }.join()
 }
